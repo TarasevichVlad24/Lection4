9bc977b18da926ee6b5992fbfeea392cc582c1e2
TarasevichVlad24 <gagagdet@gmail.com>
2022-11-29 20:22:14 +0200

    111

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int [][] matrix = new int [][] {
+                {1, 2 ,3},
+                {4, 5 ,6},
+                {7, 8, 9}};
+        System.out.println(Arrays.toString(matrix));
+        System.out.println(Arrays.deepToString(matrix));
+
+    }
+}
